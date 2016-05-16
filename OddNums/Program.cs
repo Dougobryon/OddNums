05991d20a3096fb980ab307c077605e0f6cf7809
Your Name <you.example.com>
2016-05-16 11:58:16 -0400

    OddNums

---
+++ OddNums/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print out odd numbers from 1 to 99 inclusive
+            //start at 1, make condition less than or equal to 99, add two after each loop
+            int counter;
+            for (counter = 1; counter <= 99; counter += 2)
+            {
+
+                System.Console.WriteLine(counter);
+
+            }
+
+        }
+    }
+}
